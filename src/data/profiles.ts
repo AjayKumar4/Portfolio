3cf9a5de32df56d741b480fb0ba23efbd1fa6fe2
Ajay Kumar <n.uajaykumar@gmail.com>
2023-11-09 13:51:44 +0000

    update css of image

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -2,7 +2,7 @@ export const profiles = {
   avatarSrc: './src/assets/images/profile/aj.jpeg',
   name: 'Ajay Kumar',
   title: 'DevOps Engineer',
-  avatarWidth: '100',
+  avatarWidth: '80',
   summary:
     'I am a results-driven DevOps Engineer with 7+ years of experience in automating and streamlining software development and delivery. Proven ability to work with cross-functional teams to implement and manage CI/CD pipelines, infrastructure as code, and monitoring and alerting systems.'
 }
